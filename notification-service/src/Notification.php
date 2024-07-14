c48e490c672087ca587dd6aa6093af5fcbf793e4
AyoubHoc <166333786+AyoubHoc@users.noreply.github.com>
2024-07-14 20:52:15 +0200

    Create Notification.php

---
+++ notification-service/src/Notification.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NotificationRepository")
+ */
+class Notification
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email_recipient;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sujet;
+
+    // Getters et setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmailRecipient(): ?string
+    {
+        return $this->email_recipient;
+    }
+
+    public function setEmailRecipient(string $email_recipient): self
+    {
+        $this->email_recipient = $email_recipient;
+
+        return $self;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $self;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->sujet;
+    }
+
+    public function setSujet(string $sujet): self
+    {
+        $this->sujet = $sujet;
+
+        return $self;
+    }
+}
